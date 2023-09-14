b61c82a2e276207c64c3393586cd3f04994d3466
1099255210 <wudi7mi@gmail.com>
2023-09-15 00:43:09 +0800

    修改base为crowdcompetition

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/catest/",
+  base: "/crowdcompetition/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

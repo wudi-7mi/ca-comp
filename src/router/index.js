b97d1caa87ca3d78da7ce2e45773b26c8f1f6c69
wudi_7mi <122330888+wudi-7mi@users.noreply.github.com>
2023-09-22 12:02:28 +0800

    20230922 12:02 fix

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,6 @@ import Settings from '../views/Settings.vue'
 import Submit from '../views/Submit.vue'
 import Test from '../views/Test.vue'
 
-import Question01 from '../views/questions/question01.vue'
-import Question02 from '../views/questions/question02.vue'
-import Question03 from '../views/questions/question03.vue'
-import Question04 from '../views/questions/question04.vue'
-import Question05 from '../views/questions/question05.vue'
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -28,28 +22,6 @@ const router = createRouter({
       path: '/Settings',
       name: 'settings',
       component: Settings,
-      children: [
-        {
-          path: 'question01',
-          component: Question01
-        },
-        {
-          path: 'question02',
-          component: Question02
-        },
-        {
-          path: 'question03',
-          component: Question03
-        },
-        {
-          path: 'question04',
-          component: Question04
-        },
-        {
-          path: 'question05',
-          component: Question05
-        },
-      ]
     },
     {
       path: '/Submit',

d596b410a1ccc3d850b562383ab39c61d02232af
wudi_7mi <122330888+wudi-7mi@users.noreply.github.com>
2023-09-22 16:03:54 +0800

    fixed

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,31 +1,4 @@
 <template>
   <main>
-    <div class="bg-body-tertiary p-5 rounded text-center">
-
-      <p class="content-title">作品提交指南</p>
-      <p class="lead">本次大赛作品提交在<a href="http://weblog.crowdhmt.com/">CrowdHMT人机物融合群智计算系统开放平台</a>进行</p> 
-      <p class="lead">
-        所有参赛成员必须先在该平台完成注册，并填写在线表格登记 
-        <a href="http://weblog.crowdhmt.com/register">【注册链接】</a>
-        <a href="http://docs.qq.com/sheet/DWFRaQWhJWERRSlBk?tab=BB08J2">【在线表格】</a>
-      </p>
-      
-      <img src="/register.png" alt="注册" class="img-fluid rounded">
-      
-      <p class="lead">作品提交请在平台上点击【文章】-【立即撰写】发帖</p> 
-      <img src="/write.png" alt="编写" class="img-fluid rounded">
-      <p class="lead">帖子标题格式：【赛题n】-队伍名，并在正文部分添加附件，最后发布文章</p> 
-      <img src="/submit.png" alt="提交" class="img-fluid rounded">
-    </div>
   </main>
 </template>
-
-<style scoped>
-  img {
-    width: 75%;
-    margin: 20px;
-  }
-  p {
-    margin: 20px;
-  }
-</style>

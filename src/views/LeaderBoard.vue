2b96c862356551d389e869ecd67c255bb3692b5c
wudi7mi <wudi7mi@gmail.com>
2023-11-08 12:38:56 +0800

    第一次分数更新

--- src/views/LeaderBoard.vue
+++ src/views/LeaderBoard.vue
@@ -20,10 +20,10 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="i in 20" :key="i">
-        <td>{{ TeamScore[0].name }}</td>
-        <td>{{ TeamScore[0].score }}</td>
-        <td>{{ TeamScore[0].rank }}</td>
+      <tr v-for="(t, index) in currentScore" :key="index">
+        <td>{{ t.name }}</td>
+        <td>{{ t.score }}</td>
+        <td>{{ t.rank }}</td>
       </tr>
     </tbody>
   </table>
@@ -34,8 +34,19 @@ export default {
   data () {
     return {
       TeamScore: [
-        { name: "TBD", score: "TBD", rank: "TBD" }
+        [
+          { name: "深挖特征队", score: "0.809867769", rank: "1" },
+          { name: "一周乐园队", score: "0.808127064", rank: "2" },
+          { name: "行则将至", score: "0.798387097", rank: "3" },
+          { name: "数据科学基础2023秋季", score: "0.759138229", rank: "4" },
+          { name: "睡大觉", score: "0.754941319", rank: "5" },
+        ],
+        [],
+        [],
+        [],
+        []
       ],
+      currentScore: [],
       breadcrumbItems: [
         { label: '赛题1', status: { 'breadcrumb-item': true } },
         { label: '赛题2', status: { 'breadcrumb-item': true } },
@@ -49,7 +60,25 @@ export default {
   methods: {
     switchLeaderBoard(index) {
       this.currentLabel = this.breadcrumbItems[index].label
+      this.swapScore(index)
+    },
+    swapScore(index) {
+      if (this.TeamScore[index].length == 0) {
+        this.currentScore = []
+        for (let i = 0; i < 20; i++) {
+          this.currentScore.push({ name: "TBD", score: "TBD", rank: "TBD" })
+        }
+      } else {
+        this.currentScore = this.TeamScore[index]
+      }
+      console.log({
+        "index": index,
+        "current": this.currentScore
+      })
     }
+  },
+  mounted() {
+    this.swapScore(0)
   }
 }
 </script>
